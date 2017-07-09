07bacba54a35a3e01d7b6ce8fe24932bf6f5c1ef
Lena <etarnovskaya@gmail.com>
2017-07-09 13:44:36 +0300

    create contact and groupp usind lambda

--- addressbook-web-tests/src/test/java/ru/stqa/pdt/adressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pdt/adressbook/tests/ContactCreationTests.java
@@ -3,7 +3,9 @@ package ru.stqa.pdt.adressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pdt.adressbook.model.ContactData;
+import ru.stqa.pdt.adressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,7 +16,7 @@ public class ContactCreationTests extends TestBase {
     app.getNavigationHelper().goToHomePage();
     List<ContactData> before = app.getContactHelper().getContactList();
     ContactData contact = new ContactData("fname", "mName",
-            "lName", "Moscow", "999999999", "etarnovskaya@gmail.com",
+            "lName1", "Moscow", "999999999", "etarnovskaya@gmail.com",
             "wow notes", "new1");
     //int before = app.getContactHelper().getContactCount();
     app.getContactHelper().initContactCreation();
@@ -23,15 +25,16 @@ public class ContactCreationTests extends TestBase {
     List<ContactData> after = app.getContactHelper().getContactList();
 
     //int after = app.getContactHelper().getContactCount();
-    Assert.assertEquals(after.size(), before.size()+1);
-    int max =1;
-    for (ContactData c: after){
-      if(contact.getId()> max){
-        max= c.getId();
-      }
-    }
+//    Assert.assertEquals(after.size(), before.size()+1);
+//    int max =1;
+//    for (ContactData c: after){
+//      if(contact.getId()> max){
+//        max= c.getId();
+//      }
+//    }
 
-
+    Comparator<? super ContactData> ById =(o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
 
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
--- addressbook-web-tests/src/test/java/ru/stqa/pdt/adressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pdt/adressbook/tests/GroupCreationTests.java
@@ -1,9 +1,11 @@
 package ru.stqa.pdt.adressbook.tests;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import ru.stqa.pdt.adressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,19 +15,21 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getNavigationHelper().goToGroupPage();
-    GroupData group = new GroupData("new1", null, null);
+    GroupData group = new GroupData("new2", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
-    int max=0;
-    for(GroupData g: after){
+//    int max=0;
+//    for(GroupData g: after){
+//
+//      if(group.getId()>max){
+//        max = g.getId();
+//      }
+//    }
+//    group.setId(max);
 
-      if(group.getId()>max){
-        max = g.getId();
-      }
-    }
-    group.setId(max);
-
+    Comparator<? super GroupData> ById =(o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
